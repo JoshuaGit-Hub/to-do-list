5236c2e1f79d63b7ea04eb658e48762ff9653857
JoshuaGit-Hub <150089591+JoshuaGit-Hub@users.noreply.github.com>
2025-10-01 16:06:01 -0600

    Revamp to-do list UI and add delete functionality
    Updated index.html to improve input layout and list structure. Enhanced script.js to support adding tasks with checkboxes, custom styling, and a trash icon for deletion. Added trash icon image and improved styles in style.css for better user experience, including checkbox and input styling.

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
 const lista = document.getElementById('container-list');
 const input = document.getElementById('input');
-let valueInput;
 
 input.addEventListener("keydown", (e) => {
+    if(e.key === 'Enter' && input.value.trim() !== ""){
+        lista.style.opacity = '1';
 
-    if(e.key === 'Enter'){
+        const text = input.value;
+        const primeiraLetra = text.charAt(0).toUpperCase();
+        const textResto = text.slice(1);
+        const valueInput = primeiraLetra + textResto;
 
-        valueInput = input.value;
-        
         const novoLi = document.createElement('li');
         lista.appendChild(novoLi);
         
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
+        const label = document.createElement('label');
+        label.classList.add('checkbox');
 
-        novoLi.textContent = valueInput;
+        const inputCheck = document.createElement('input');
+        inputCheck.type = 'checkbox';
+        label.appendChild(inputCheck);
 
-        novoLi.appendChild(checkbox);
+        const spanBox = document.createElement('span');
+        spanBox.classList.add('caixa');
+        label.appendChild(spanBox);
+
+                
+        const spanTexto = document.createElement('span');
+        spanTexto.classList.add('texto');
+        spanTexto.style.cursor = 'pointer';
+        spanTexto.textContent = valueInput;
+        label.appendChild(spanTexto);
+        
+        
+        const img = document.createElement('img');
+        img.classList.add('trashStyle');
+        img.src = 'images/trash.png';
+        img.width = 20;
+        img.style.filter = 'invert(1)';
+
+        const spanTrash = document.createElement('span');
+        spanTrash.appendChild(img);
+        spanTrash.style.marginLeft = '30px';
+        spanTrash.style.cursor = 'pointer';
+        spanTrash.classList.add('hovered');
+
+        spanTrash.addEventListener('click', () => {
+
+            spanTexto.remove();
+            spanBox.remove();
+            inputCheck.remove();
+            spanTrash.remove();
+            novoLi.remove();
+
+            if(lista.hasChildNodes()){
+                lista.style.display = 'none';
+            }
+        });
+
+        label.appendChild(spanTrash);
+        
+
+        novoLi.appendChild(label);
+        input.value = "";
     }
 });
